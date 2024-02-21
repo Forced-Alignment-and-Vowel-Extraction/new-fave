71f334335e3bd2dddf53c8faa1287f4cba5b5c2b
JoFrhwld <JoFrhwld@gmail.com>
2024-02-21 16:48:05 -0500

    Initializing gh-pages branch

---
+++ src/new_fave/extract.py
@@ -0,0 +1,15 @@
+from fasttrackpy import process_audio_file, \
+    process_directory, \
+    process_audio_textgrid,\
+    process_corpus
+from aligned_textgrid import AlignedTextGrid,\
+    custom_classes
+
+from pathlib import Path
+
+
+def extract():
+    """_summary_
+    """
+    pass
+
