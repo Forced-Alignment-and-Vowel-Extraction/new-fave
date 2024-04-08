38304fe38ea074f389c1883f3a60e7e293f531a2
JoFrhwld <JoFrhwld@gmail.com>
2024-04-08 18:47:21 -0400

    initial cli worl

--- src/new_fave/extract.py
+++ src/new_fave/extract.py
@@ -1,7 +1,4 @@
-from fasttrackpy import process_audio_file, \
-    process_directory, \
-    process_audio_textgrid,\
-    process_corpus
+from fasttrackpy import process_corpus
 
 from aligned_textgrid import AlignedTextGrid,\
     custom_classes
@@ -27,18 +24,91 @@ from new_fave.utils.textgrid import get_textgrid, \
 
 from new_fave.utils.local_resources import recodes,\
     parsers, \
-    measurements
+    measurements,\
+    fasttrack_config
 
 from new_fave.speaker.speaker import Speaker
 
 from pathlib import Path
 import click
 import cloup
+from cloup import Context, HelpFormatter, HelpTheme, Style,\
+    option_group, option
+
 import yaml
 
+import inspect
 
-def fave_extract():
+
+@cloup.command()
+@cloup.argument(
+    "corpus_path",
+    help="Path to a corpus",
+    type = click.Path(
+        exists=True,
+        dir_okay=True,
+        file_okay=False,
+        readable=True
+    )
+)
+@option_group(
+    "Configs",
+    "Config files",
+    option(
+        "--fasttrack_config",
+        default=fasttrack_config["default"],
+        type=click.File(mode='r')
+    ),
+    option(
+        "--labelset_parser",
+        default=parsers["cmu_parser"],
+        type=click.Path(exists=True,
+            file_okay=True,
+            dir_okay=False,
+            readable=True)
+    ),
+    option(
+        "--recode_rules",
+        default=recodes["cmu2labov"],
+        type=click.Path(exists=True)
+    ),
+    option(
+        "--measurement_points",
+        default = measurements["fave"],
+        type=click.Path(
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            readable=True
+        )
+    )
+)
+
+def fave_extract(
+    corpus_path: Path,
+    fasttrack_config: Path,
+    labelset_parser: Path,
+    recode_rules: Path,
+    measurement_points: Path,
+    **kwargs
+):
     """_summary_
     """
-    pass
 
+    
+    fast_track_args = yaml.safe_load(fasttrack_config)
+    corpus_args = {
+        x:fast_track_args[x]
+        for x in inspect.signature(process_corpus).parameters
+        if x in fast_track_args
+    }
+
+    vms = process_corpus(
+        corpus_path=corpus_path,
+        **corpus_args
+    )
+
+    click.echo(f"{len(vms)}")
+
+if __name__ == "__main__":
+    fave_extract()
