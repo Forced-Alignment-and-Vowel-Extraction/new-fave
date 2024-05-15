d5a0107ab0ce28e75003c544983f396b459ff523
JoFrhwld <JoFrhwld@gmail.com>
2024-05-15 13:35:14 -0400

    generic resource resolver

--- src/new_fave/utils/local_resources.py
+++ src/new_fave/utils/local_resources.py
@@ -1,4 +1,6 @@
 from importlib.resources import files
+from pathlib import Path
+from typing import Callable, Any
 
 fave_fasttrack = str(files("new_fave").joinpath("resources", "fasttrack_config.yml"))
 
@@ -25,3 +27,50 @@ heuristics = {
 fasttrack_config = {
     "default": fave_fasttrack
 }
+
+def generic_resolver(
+        resolve_func: Callable, 
+        to_resolve: str|Path = None,
+        resource_dict: dict = None,
+        default_value:Any =  None):
+    """
+    Resolve a passed string or path, and 
+    return the desired object
+
+    Args:
+        resolve_func (Callable):
+            The function to apply to the file.
+        to_resolve (str | Path, optional): 
+            The value to resolve. Either a name of 
+            a built-in resource, or a path to a config file.
+            Defaults to None.
+        resource_dict (dict, optional): 
+            A dictionary defining providing paths to 
+            a built-in resource. 
+            Defaults to None.
+        default_value (Any, optional):
+            A default value to return.
+            Defaults to None.
+
+    Returns:
+        (Any): The value returned by resolve_func
+    """
+    if not to_resolve:
+        return default_value
+
+    if to_resolve in resource_dict:
+        return resolve_func(resource_dict[to_resolve])
+    
+    if type(to_resolve) is str:
+        to_resolve = Path(to_resolve)
+    
+    if to_resolve.exists() and to_resolve.is_file():
+        return resolve_func(to_resolve)
+    
+    raise ValueError(
+        (
+            f"The provided value {to_resolve} "
+            "is not a built-in resource "
+            "and does not appear to be a file."
+        )
+    )
