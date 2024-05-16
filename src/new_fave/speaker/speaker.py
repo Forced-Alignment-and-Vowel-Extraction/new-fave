52b2b13a9cbf15a33bf168010922b5591166a3d2
JoFrhwld <JoFrhwld@gmail.com>
2024-05-16 15:28:14 -0400

    ensure file_name is the stem

--- src/new_fave/speaker/speaker.py
+++ src/new_fave/speaker/speaker.py
@@ -25,18 +25,34 @@ read_method = {
 }
 class Speaker():
     def __init__(self, arg: dict|list|pl.DataFrame|Path = None):
+        self.df = None
+        if type(arg) is str:
+            arg = Path(arg)
         if isinstance(arg, dict) or isinstance(arg, list):
             self.df = pl.DataFrame(arg)
-            return
         if isinstance(arg, pl.DataFrame):
             self.df = arg
-            return
         if isinstance(arg, Path):
             self.df = self.read_speaker(arg)
-            return
         
-        self.df = None
+        if not self.df is None:
+            self.df = (
+                self.df
+                .with_columns(
+                    pl.col("file_name").map_elements(self._get_stem, return_dtype=pl.String)
+                )
+            )
+       
     
+    def __repr__(self) -> str:
+        out = "Speaker data \n" + self.df.__repr__()
+        return out
+    
+    def _get_stem(self, x):
+        out = Path(x).stem
+        return out
+
+
     def read_speaker(self, path:Path):
         extension = path.suffix
         reader = read_method[extension]
