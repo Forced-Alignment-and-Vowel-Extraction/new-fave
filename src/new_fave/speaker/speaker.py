4f090c865eb7c805031b71c4d4edb02bb26071a4
JoFrhwld <JoFrhwld@gmail.com>
2024-05-16 16:18:53 -0400

    parse old style fave speaker file

--- src/new_fave/speaker/speaker.py
+++ src/new_fave/speaker/speaker.py
@@ -2,7 +2,9 @@ import polars as pl
 from pathlib import Path
 import yaml
 import csv
-
+import warnings
+from glob import glob
+import argparse
 
 def yaml2speaker(path: Path) -> pl.DataFrame:
     with path.open() as f:
@@ -18,10 +20,49 @@ def excel2speaker(path: Path) -> pl.DataFrame:
     df = pl.read_excel(path)
     return df
 
+def oldfave2speaker(path: Path) -> pl.DataFrame:
+    speaker_parser = argparse.ArgumentParser(description="parses a .speaker file",
+                                     fromfile_prefix_chars="+")
+    speaker_parser.add_argument("--name")
+    speaker_parser.add_argument("--first_name")
+    speaker_parser.add_argument("--last_name")
+    speaker_parser.add_argument("--age")
+    speaker_parser.add_argument("--sex")
+    speaker_parser.add_argument("--ethnicity")
+    speaker_parser.add_argument("--years_of_schooling")
+    speaker_parser.add_argument("--location")
+    speaker_parser.add_argument("--city")
+    speaker_parser.add_argument("--state")
+    speaker_parser.add_argument("--year")
+    speaker_parser.add_argument("--speakernum")
+    speaker_parser.add_argument("--tiernum")
+    speaker_parser.add_argument("--vowelSystem",
+        choices = ['phila', 'Phila', 'PHILA', 'NorthAmerican', 'simplifiedARPABET'])
+
+    speaker_opts = speaker_parser.parse_args(["+"+str(path)])
+    speaker_opts_dict = speaker_opts.__dict__
+    speaker_opts_dict = {
+        k:speaker_opts_dict[k]
+        for k in speaker_opts_dict
+        if speaker_opts_dict[k]
+    }
+
+
+    df = pl.DataFrame(speaker_opts_dict)
+    df = (
+        df
+        .with_columns(
+            file_name = pl.lit(path.stem),
+            speaker_num = pl.col("tiernum").str.parse_int()
+        )
+    )
+    return df
+
 read_method = {
     ".yml": yaml2speaker,
     ".csv": csv2speaker,
-    ".xlsx": excel2speaker
+    ".xlsx": excel2speaker,
+    ".speaker": oldfave2speaker
 }
 class Speaker():
     def __init__(self, arg: dict|list|pl.DataFrame|Path = None):
@@ -35,7 +76,25 @@ class Speaker():
         if isinstance(arg, Path):
             self.df = self.read_speaker(arg)
         
-        if not self.df is None:
+        if not "file_name" in self.df.columns:
+            warnings.warn(
+                ( 
+                    "The provided speaker file does not contain "
+                    "a file_name field. "
+                    "Metadata won't be correctly merged."
+                )
+            )
+        
+        if not "speaker_num" in self.df.columns:
+            warnings.warn(
+                ( 
+                    "The provided speaker file does not contain "
+                    "a speaker_num field. "
+                    "Metadata won't be correctly merged."
+                )
+            )        
+        
+        if not self.df is None and "file_name" in self.df.columns:
             self.df = (
                 self.df
                 .with_columns(
