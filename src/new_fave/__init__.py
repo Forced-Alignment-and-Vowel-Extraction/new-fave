c25dd8aa775c6dadbfd47178780f4e0c9f490760
JoFrhwld <JoFrhwld@gmail.com>
2024-05-16 12:53:38 -0400

    object export

---
+++ src/new_fave/__init__.py
@@ -0,0 +1,12 @@
+from new_fave.measurements.vowel_measurement import VowelMeasurement, \
+    VowelClass, \
+    VowelClassCollection, \
+    SpeakerCollection
+
+
+__all__ = [
+    "VowelMeasurement", 
+    "VowelClass", 
+    "VowelClassCollection", 
+    "SpeakerCollection"
+]
