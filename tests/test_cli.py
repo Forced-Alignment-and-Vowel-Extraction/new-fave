5d7cc64d48c9ad327c4866b7f23c34620046acbe
JoFrhwld <JoFrhwld@gmail.com>
2024-05-21 16:53:51 -0400

    cli tests

---
+++ tests/test_cli.py
@@ -0,0 +1,130 @@
+from new_fave.extract import fave_extract
+from pathlib import Path
+from click.testing import CliRunner
+import pytest
+import yaml
+import tempfile
+import logging
+
+def test_audio_textgrid():
+    tmp = tempfile.TemporaryDirectory()
+    tmp_path = Path(tmp.name)
+
+
+    audio_path = Path("tests", "test_data", "corpus", "josef-fruehwald_speaker.wav")
+    textgrid_path = Path("tests", "test_data", "corpus", "josef-fruehwald_speaker.TextGrid")
+    ft_config = Path("tests", "test_patterns", "test_ft_config.yml")
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        fave_extract,
+        [
+            "audio-textgrid",
+            str(audio_path),
+            str(textgrid_path),
+            "--destination", tmp.name,
+            "--ft-config", str(ft_config)
+        ]
+    )
+
+    assert result.exit_code == 0, result.output
+    csvs = list(tmp_path.glob("*.csv"))
+    assert len(csvs) > 0
+
+    result = runner.invoke(
+        fave_extract,
+        [
+            "audio-textgrid",
+            str(audio_path),
+            str(textgrid_path),
+            "--destination", tmp.name,
+            "--ft-config", str(ft_config)
+        ],
+        input='n\nn\n'
+    )
+
+    assert result.exit_code == 0, result.output
+    tmp.cleanup()
+
+def test_corpus():
+    tmp = tempfile.TemporaryDirectory()
+    tmp_path = Path(tmp.name)
+
+
+    corpus_path = Path("tests", "test_data", "corpus")
+    ft_config = Path("tests", "test_patterns", "test_ft_config.yml")
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        fave_extract,
+        [
+            "corpus",
+            str(corpus_path),
+            "--destination", tmp.name,
+            "--ft-config", str(ft_config)
+        ]
+    )
+
+    assert result.exit_code == 0, result.output
+    csvs = list(tmp_path.glob("*.csv"))
+    assert len(csvs) > 0
+
+
+    result = runner.invoke(
+        fave_extract,
+        [
+            "corpus",
+            str(corpus_path),
+            "--destination", tmp.name,
+            "--ft-config", str(ft_config)
+        ],
+        input='n\nn\n'
+    )
+
+    assert result.exit_code == 0, result.output
+    tmp.cleanup()
+
+
+def test_subcorpora():
+    tmp = tempfile.TemporaryDirectory()
+    tmp_path = Path(tmp.name)
+
+
+    corpus_path1 = Path("tests", "test_data", "subcorpora", "josef-fruehwald"),
+    corpus_path2 = Path("tests", "test_data", "subcorpora", "KY25A")
+    ft_config = Path("tests", "test_patterns", "test_ft_config.yml")
+
+    runner = CliRunner()
+
+    result = runner.invoke(
+        fave_extract,
+        [
+            "subcorpora",
+            str(corpus_path1),
+            str(corpus_path2),
+            "--destination", tmp.name,
+            "--ft-config", str(ft_config)
+        ]
+    )
+
+    assert result.exit_code == 0, result.output
+    csvs = list(tmp_path.glob("*.csv"))
+    assert len(csvs) > 0
+
+
+    result = runner.invoke(
+        fave_extract,
+        [
+            "subcorpora",
+            str(corpus_path1),
+            str(corpus_path2),
+            "--destination", tmp.name,
+            "--ft-config", str(ft_config)
+        ],
+        input='n\nn\n'
+    )
+
+    assert result.exit_code == 0, result.output
+    tmp.cleanup()
