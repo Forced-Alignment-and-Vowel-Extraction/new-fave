2f0af09d662cb53c6006cad6600894b03a498fd8
JoFrhwld <JoFrhwld@gmail.com>
2024-11-04 19:10:03 -0500

    add __version__

--- src/new_fave/__init__.py
+++ src/new_fave/__init__.py
@@ -10,6 +10,8 @@ from new_fave.patterns.writers import write_data, pickle_speakers, unpickle_spea
 
 from importlib.metadata import version
 
+__version__ = version("new_fave")
+
 __all__ = [
     "VowelMeasurement", 
     "VowelClass", 
