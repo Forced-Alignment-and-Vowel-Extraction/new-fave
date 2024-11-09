f78d5c01cd1b0af679863274493ea385a32b9ce3
JoFrhwld <JoFrhwld@gmail.com>
2024-11-09 17:14:22 -0500

    simplify to just poetry

--- src/new_fave/__init__.py
+++ src/new_fave/__init__.py
@@ -19,10 +19,7 @@ pyproject_toml_file = Path(__file__).parent.parent.parent / "pyproject.toml"
 if pyproject_toml_file.exists() and pyproject_toml_file.is_file():
     data = toml.load(pyproject_toml_file)
     # check project.version
-    if "project" in data and "version" in data["project"]:
-        __version__ = data["project"]["version"]
-    # check tool.poetry.version
-    elif "tool" in data and "poetry" in data["tool"] and "version" in data["tool"]["poetry"]:
+    if "tool" in data and "poetry" in data["tool"] and "version" in data["tool"]["poetry"]:
         __version__ = data["tool"]["poetry"]["version"]
 
 __all__ = [
